16ef6cdc5e477a492859a9ff3741e56511d4072a
unknown <arjitsingh13.com>
2024-07-16 17:27:06 +0530

    commiting db file again

--- WEEK-5/Todo_app/backend/db.js
+++ WEEK-5/Todo_app/backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect('mongodb+srv://arjitsingh13mongodb:n9W%40htt_mi%40Rdv8@cluster0.pdygrde.mongodb.net/TODO_DB');
+mongoose.connect('mongodb+srv://arjitsingh13mongodb:XXXXXXXXXX@cluster0.pdygrde.mongodb.net/TODO_DB');
 
 
 const  todoSchema = new mongoose.Schema({
